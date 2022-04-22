de8a1a19918d80d3fa6b1d3ebf9645bc9099abf0
Lee Scott <37646873+Lee-Scott@users.noreply.github.com>
2022-04-22 16:31:25 -0700

    Add files via upload

---
+++ PhotoAppDescovereyService/src/test/java/com/familyfirstsoftware/PhotoAppDescovereyService/PhotoAppDescovereyServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.familyfirstsoftware.PhotoAppDescovereyService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PhotoAppDescovereyServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
